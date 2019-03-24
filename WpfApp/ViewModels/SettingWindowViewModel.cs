86463eac4b828ed2f5c5b52e7487a468d6ae5017
Alexander Borovskikh <borislav_god@mail.ru>
2019-03-24 19:27:26 +0500

    настройка одного из разделов для выбора темы оформления

--- WpfApp/ViewModels/SettingWindowViewModel.cs
+++ WpfApp/ViewModels/SettingWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
 
 namespace WpfApp.ViewModels
 {
@@ -12,9 +16,57 @@ namespace WpfApp.ViewModels
             set { Set(nameof(Title), ref _title, value); }
         }
 
+        private IEnumerable<Swatch> _swatches;
+
+        public IEnumerable<Swatch> Swatches
+        {
+            get { return _swatches; }
+            set { Set(nameof(Swatches), ref _swatches, value); }
+        }
+
         public SettingWindowViewModel()
         {
             Title = "Основные настройки программы";
+            Swatches = new SwatchesProvider().Swatches;
+        }
+
+        private RelayCommand<Swatch> _applyPrimaryCommand;
+
+        public RelayCommand<Swatch> ApplyPrimaryCommand
+        {
+            get
+            {
+                return _applyPrimaryCommand ?? (_applyPrimaryCommand = new RelayCommand<Swatch>((swatch) =>
+                {
+                    new PaletteHelper().ReplacePrimaryColor(swatch);
+                }));
+            }
+        }
+
+        private RelayCommand<Swatch> _applyAccentCommand;
+
+        public RelayCommand<Swatch> ApplyAccentCommand
+        {
+            get
+            {
+                return _applyAccentCommand ?? (_applyAccentCommand = new RelayCommand<Swatch>((swatch) =>
+                {
+                    new PaletteHelper().ReplaceAccentColor(swatch);
+                }));
+            }
+        }
+
+        private RelayCommand<bool> _changeThemeCommand;
+
+        public RelayCommand<bool> ChangeThemeCommand
+        {
+            get
+            {
+                return _changeThemeCommand ?? (_changeThemeCommand = new RelayCommand<bool>((dark) =>
+                {
+                    new PaletteHelper().SetLightDark(dark);
+                }));
+            }
         }
     }
 }
