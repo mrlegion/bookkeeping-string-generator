70bce86d2e213ce8cfd4d91ae0f5d6140ba83bc5
Alexander Borovskikh <borislav_god@mail.ru>
2019-03-24 17:29:45 +0500

    настройка панели вкладок на окне параметров

--- WpfApp/ViewModels/SettingWindowViewModel.cs
+++ WpfApp/ViewModels/SettingWindowViewModel.cs
@@ -4,6 +4,17 @@ namespace WpfApp.ViewModels
 {
     public class SettingWindowViewModel : ViewModelBase
     {
+        private string _title;
 
+        public string Title
+        {
+            get { return _title; }
+            set { Set(nameof(Title), ref _title, value); }
+        }
+
+        public SettingWindowViewModel()
+        {
+            Title = "Основные настройки программы";
+        }
     }
 }
