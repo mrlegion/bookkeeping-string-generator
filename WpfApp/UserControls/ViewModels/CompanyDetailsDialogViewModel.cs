63fff818da5affb99dbde2ca2ae527cc2067e003
Alexander Borovskikh <borislav_god@mail.ru>
2019-03-26 22:23:04 +0500

    новый стиль оформления диалогового окна отображения информации у окна просмотра компаний

--- WpfApp/UserControls/ViewModels/CompanyDetailsDialogViewModel.cs
+++ WpfApp/UserControls/ViewModels/CompanyDetailsDialogViewModel.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
-
 using CommonServiceLocator;
-
 using Domain.Services;
-
 using Infrastructure.Entities;
 
 namespace WpfApp.UserControls.ViewModels
 {
     public class CompanyDetailsDialogViewModel : UserControlViewModelBase<Company>
     {
+        #region Fields
+
         private IEnumerable<Organization> _organizations;
+        private IDictionary<string, string> _general;
+
+        #endregion
+
+        #region Properties
 
         public IEnumerable<Organization> Organizations
         {
             get { return _organizations; }
             set { Set(nameof(Organizations), ref _organizations, value); }
         }
+
+        public IDictionary<string, string> General
+        {
+            get { return _general; }
+            set { Set(nameof(General), ref _general, value); }
+        }
+
+        #endregion
+
+        #region Implimentations
 
         public override void Init(Company entity)
         {
@@ -29,6 +43,15 @@ namespace WpfApp.UserControls.ViewModels
                 var service = ServiceLocator.Current.GetInstance<OrganizationService>();
                 Organizations = service.GetOrganizationByCompanyId(Entity.Id);
             });
+
+            General = new Dictionary<string, string>()
+            {
+                { "Наименование компании:", Entity.Name },
+                { "Номер ИНН компании:", Entity.Inn },
+                { "Номер КПП компании:", Entity.Kpp },
+            };
         }
+
+        #endregion
     }
 }
